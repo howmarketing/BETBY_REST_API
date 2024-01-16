0419e571b037da962f11925b9b59b9fcb73d75b3
Gabriel Ariza Gomes de Castro <gabriel@arizapp.com.br>
2024-01-16 00:54:25 -0300

    feat(unit-test): Creating the initial unit tests coverage

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+let server: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  server = await app.listen(3000);
 }
+
+async function closeServer() {
+  if (server) {
+    await server.close();
+  }
+}
+
 bootstrap();
+
+export { closeServer };
